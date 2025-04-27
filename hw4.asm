aa9708e27466f4b4c6d78d374b967affe6b353da
Yusuf Badar <ybadar13@gmail.com>
2025-04-27 03:55:55 +0000

    WORK

--- hw4.asm
+++ hw4.asm
@@ -99,87 +99,98 @@ search_node_end:
 	jr $ra
 
 # Function: insert_node
-# Arguments: 
-#   $a0 - pointer to root
-#   $a1 - value to insert
-# Returns: 
+# Arguments:
+#    $a0 - pointer to root
+#    $a1 - value to insert
+# Returns:
 #	$v0 - pointer to root
 
 insert_node:
-	# Function prologue
-    addi $sp, $sp, -8
-    sw $ra, 4($sp)
-    sw $s1, 0($sp)
-    move $s1, $a0
-    li $v0,9
-    li $a0,20
-    syscall
-    move $a0, $s1
-    move $t0,$v0
-    sw $a1,0($t0)
-    li $t1,1
-    sw $t1,12($t0)
-    sw $zero,4($t0)
-    sw $zero,8($t0)
-    sw $zero,16($t0)
-    move $t1,$zero
-    move $t2,$s1
+    addi $sp, $sp, -8
+    sw $ra, 4($sp)
+    sw $s1, 0($sp)
+    move $s1, $a0
+    li $v0,9
+    li $a0,20
+    syscall
+    move $a0, $s1
+    move $t0,$v0
+    sw $a1,0($t0)
+    li $t1,1
+    sw $t1,12($t0)
+    sw $zero,4($t0)
+    sw $zero,8($t0)
+    sw $zero,16($t0)
+
+    move $t1,$zero
+    move $t2,$s1
+
 bst_loop:
-    beqz $t2,bst_done
-    move $t1,$t2
-    lw $t3,0($t2)
-    blt $a1,$t3,bst_left
-    lw $t2,8($t2)
-    j bst_loop
+    beqz $t2,bst_done
+    move $t1,$t2
+    lw $t3,0($t2)
+    blt $a1,$t3,bst_left
+    lw $t2,8($t2)
+    j bst_loop
+
 bst_left:
-    lw $t2,4($t2)
-    j bst_loop
+    lw $t2,4($t2)
+    j bst_loop
+
 bst_done:
-    sw $t1,16($t0)
-    beqz $t1,new_root
-    lw $t3,0($t1)
-    blt $a1,$t3,link_L
-    sw $t0,8($t1)
-    j after_ins
+    sw $t1,16($t0)
+    beqz $t1,new_root
+    lw $t3,0($t1)
+    blt $a1,$t3,link_L
+    sw $t0,8($t1)
+    j after_ins
+
 link_L:
-    sw $t0,4($t1)
+    sw $t0,4($t1)
+
 new_root:
-    move $a0,$t0
+    move $a0,$t0
+
 after_ins:
-    move $a0,$t0
-    jal insert_fixup
-    move $v0,$a0
-    j insert_node_done
+    move $a0,$t0
+    jal insert_fixup
+    move $v0,$a0
+    j insert_node_done
 
 insert_node_done:
-	#Function Epilogue
-    lw $s1, 0($sp)
-    lw $ra, 4($sp)
-    addi $sp, $sp, 8
-    jr $ra
+    lw $s1, 0($sp)
+    lw $ra, 4($sp)
+    addi $sp, $sp, 8
+    jr $ra
+
+# Function: insert_fixup
+# Arguments:
+#    $a0 - pointer to the newly inserted node
+# Returns:
+#	$v0 - pointer to the root of the tree
 
 insert_fixup:
     addi $sp, $sp, -4
-    sw $ra, 0($sp)
+    sw $ra, 0($sp) 
     move $t2,$a0
+
 fix_loop:
     lw $t1,16($t2)
     beqz $t1,done_fix
     lw $t3,12($t1)
-    beqz $t3,done_fix
-    lw $t4,16($t1)
-    lw $t5,4($t4)
-    beq $t5,$t1,case_L
+    beqz $t3,done_fix  
+    lw $t4,16($t1) 
+    lw $t5,4($t4)  
+    beq $t5,$t1,parent_is_left 
     lw $t6,4($t4)
-    beqz $t4, done_fix
-    j chk_uncle
-    j done_fix
-case_L:
+    j check_uncle_color 
+parent_is_left:
     lw $t6,8($t4)
-chk_uncle:
+
+check_uncle_color:
     beqz $t6,do_rot
     lw $t7,12($t6)
-    beqz $t7,do_rot
+    beqz $t7,do_rot 
     li $t8,0
     sw $t8,12($t1)
     sw $t8,12($t6)
@@ -187,60 +198,89 @@ chk_uncle:
     sw $t8,12($t4)
     move $t2,$t4
     j fix_loop
+
 do_rot:
+    lw $t5,4($t1)
+    beq $t2,$t5, case_3b_left_zag
     lw $t5,4($t4)
-    beq $t5,$t1,rot_L
-    move $a0,$t1
-    jal rotate_right
-    j rot_swap
-rot_L:
-    move $a0,$t1
-    jal rotate_left
-rot_swap:
+    beq $t1,$t5, case_3b_left_parent
     move $a0,$t4
     jal rotate_left
+    j color_swap_after_rot
+
+case_3b_left_zag:
+    lw $t5,4($t4)
+    beq $t1,$t5, case_3c_left_zig
+
+    move $a0,$t1
+    jal rotate_right
+    move $t2,$a0
+    j case_3c_left_zig
+
+case_3b_left_parent:
+    move $a0,$t1
+    jal rotate_left
+    move $t2,$a0
+
+case_3c_left_zig:
+    move $a0,$t4
+    jal rotate_right
+
+color_swap_after_rot:
     lw $t1,16($t2)
     lw $t4,16($t1)
     lw $t8,12($t1)
     lw $t9,12($t4)
     sw $t8,12($t4)
-    sw $t9,12($t1)
+    sw $t9,12($t1)  
+    j done_fix
+
 done_fix:
+    move $t0,$a0
+find_rt_loop_exit:
     move $t0,$a0
 find_rt:
     lw $t1,16($t0)
-    bnez $t1,find_rt
+    bnez $t1,move_up
     sw $zero,12($t0)
     move $v0,$t0
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
 
+move_up:
+    move $t0,$t1
+    j find_rt
+
 rotate_left:
     addi $sp, $sp, -4
-    sw $ra, 0($sp)
+    sw $ra, 0($sp)  
     lw $t1,8($a0)
-    beqz $t1, done_rotate_left
-    beqz $t1, rootL
+    beqz $t1, done_rotate_left  
     lw $t2,4($t1)
     sw $t2,8($a0)
     beqz $t2,skip1
     sw $a0,16($t2)
+
 skip1:
     lw $t3,16($a0)
     sw $t3,16($t1)
     beqz $t3,rootL
     lw $t4,4($t3)
-    
     beq $a0,$t4,linkpl1
-    sw  $t1,8($t3)
+    sw $t1,8($t3)
     j rl_fix
+
 linkpl1:
     sw $t1,4($t3)
+
 rl_fix:
+
 rootL:
     sw $a0,4($t1)
-    sw $t1,16($a0) 
+    sw $t1,16($a0)
+
+done_rotate_left:
     lw $ra,0($sp)
     addi $sp,$sp,4
     jr $ra
@@ -266,17 +306,13 @@ linkpl2:
     sw $t1,8($t3)
 cont2:
 rootR:
-    sw  $a0,8($t1)
-    sw  $t1,16($a0)
-    lw   $ra, 0($sp)
+    sw  $a0,8($t1)
+    sw  $t1,16($a0)
+    lw    $ra, 0($sp)
     addi $sp, $sp, 4
-    jr  $ra
+    jr  $ra
 
-done_rotate_left:
-    lw $ra, 0($sp)
-    addi $sp, $sp, 4
-    jr $ra
-done_rotate_right:
+_rotate_right:
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
